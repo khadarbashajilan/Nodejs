f05a45c5da7ad45f32108ba5e96d6dc5c56503cc
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-11-29 08:14:47 +0530

    Modules (export and import in type:module)

---
+++ 3. Modules In Depth (module type)/app.js
@@ -0,0 +1,6 @@
+// Import the 'add' and 'sub' functions from the './math.js' module
+// This allows us to use the functions defined in the 'math.js' file
+import { add, sub } from "./math.js";
+
+add(2,5)
+sub(5,4)
