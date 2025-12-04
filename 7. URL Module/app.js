65dfb90a2ac6456cce16c562e94168704132ac29
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-12-04 17:45:20 +0530

    URL module

---
+++ 7. URL Module/app.js
@@ -0,0 +1,34 @@
+// Import the URL class from the 'url' module
+import { URL } from 'url';
+
+// Create a new URL object with a sample URL string
+const myURL = new URL('https://example.com/products?category=phones&sort=price');
+
+// Log basic URL information
+console.log('Hostname:', myURL.hostname);
+console.log('Pathname:', myURL.pathname);
+console.log('Original search parameters:', myURL.searchParams);
+
+
+// Iterate through each search parameter
+// myURL.searchParams.forEach((value, name, searchParams) => {
+//     // Check if the parameter name is 'sort'
+//     if(name === 'sort'){
+//         // Remove the 'sort' parameter
+//         searchParams.delete(name);
+//         // Set a new parameter with the same name but different value
+//         searchParams.set(name, 'rating');
+//     }
+// });
+
+//OR
+
+// Update the 'sort' parameter to 'rating' if it exists
+if (myURL.searchParams.has('sort')) {
+    myURL.searchParams.set('sort', 'rating');
+}
+
+
+// Log the updated search parameters
+console.log('Updated search parameters:', myURL.searchParams);
+
